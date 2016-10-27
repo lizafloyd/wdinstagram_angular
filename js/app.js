9ea59d427bb5e91d430466d4f9802b8a7b526aa6
Liza Floyd <lizafloyd@gmail.com>
2016-10-27 07:34:47 -0400

    views and controller working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,37 @@
 "use strict";
 
+
 (function(){
-  angular.module("wdinstagram", [])
+
+  angular
+  .module("wdinstagram", [
+    "ngResource",
+    "ui.router"
+  ])
+  .config(["$stateProvider", Router])
+  .controller("indexController", [indexController])
+
+
+
+function Router($stateProvider) {
+  $stateProvider
+  .state("index", {
+    url: '/',
+    templateUrl: 'js/ng-views/index.html',
+    controller: 'indexController',
+    controllerAs: 'vm'
+  })
+
+}
+
+function indexController() {
+  console.log("index working");
+  this.dummies = [
+      {body: "hey"},
+      {body: "there"}
+    ];
+}
+
+
+
 })();
